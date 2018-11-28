a986c22b97c77c46149fc7201dfacc511f7939fc
kamil <dev.grzybek.kamil@gmail.com>
2018-11-28 15:24:19 +0100

    added friends searchbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'search_friends', to: 'users#search'
   resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: 'users#my_friends'
   resources :users, only: [:show]
