6cb3af222bd365699c4341c84d1b19a233b6b4a7
kamil <dev.grzybek.kamil@gmail.com>
2018-11-28 15:46:54 +0100

    added friends object support with restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
-  root 'home#index'
-  get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stocks', to: 'stocks#search'
-  get 'search_friends', to: 'users#search'
-  resources :user_stocks, only: [:create, :destroy]
-  get 'my_friends', to: 'users#my_friends'
+  resources :user_stocks, except: [:show, :edit, :update]
   resources :users, only: [:show]
   resources :friendships
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
+  get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
+  get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
