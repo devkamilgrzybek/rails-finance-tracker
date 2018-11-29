96d218eb8f9a76803b1a51904eb2239f381ee7e3
kamil <dev.grzybek.kamil@gmail.com>
2018-11-29 12:31:14 +0100

    added auto scroll in chat

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,10 +1,12 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {  
   received: function(data) {
     $("#messages").removeClass('hidden')
+    $('#messages').animate({scrollTop: $('#messages').prop("scrollHeight")}, 500);
+
     return $('#messages').append(this.renderMessage(data));
   },
 
   renderMessage: function(data) {
-    return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+    return "<p> <b>" + data.user + ": </b>"+new Date().toUTCString()+"</br>" + data.message + "</p>";
   }
 });
